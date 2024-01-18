2ac0c2f89205aab5d7fbf8878154568f4b0d8873
totomi2323 <tothtamas2323@gmail.com>
2024-01-18 17:26:43 +0000

    Added pagination to home_get

--- server/controllers/homeController.js
+++ server/controllers/homeController.js
@@ -1,7 +1,35 @@
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
+const Item = require("../models/item");
 
-exports.home_get = asyncHandler( async(req,res, next) => {
+const getPagination = (page, size) => {
+  const limit = size ? +size : 20;
+  const offset = page ? page * limit : 0;
 
-    res.render("home" ,{title: "Home page" , user:req.user})
-})
+  return { limit, offset };
+};
+
+exports.home_get = asyncHandler(async (req, res, next) => {
+  const { page, size, name } = req.query;
+
+  let condition = name
+    ? { name: { $regex: new RegExp(name), $options: "i" } }
+    : {};
+
+  const { limit, offset } = getPagination(page, size);
+
+  
+Item.paginate(condition, { offset, limit })
+    .then((data) => {
+        console.log(data)
+        res.render("home", { title: "Home page", user: req.user, data: data });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving tutorials.",
+      });
+    });
+
+  
+});
