4ef81a4e39b937b39e8b2adad8aee63736513bef
totomi2323 <tothtamas2323@gmail.com>
2024-02-01 18:08:08 +0000

    Home page fetches 12 items on page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,39 @@
 import React, { useEffect, useState } from "react";
+import Card from "./subComponents/Card"
+import "../styles/card.css"
 
-const Home = ()  => {
-return (
+const Home = () => {
+  const [fetchedData, setFetchedData] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/home").then((response) => {
+      response.json().then((data) => {
+        setFetchedData(data);
+      });
+    });
+  }, []);
+
+  return (
     <>
-    Home page
+      <button
+        onClick={() => {
+          console.log(fetchedData);
+        }}
+      >
+        Console
+      </button>
+      {typeof fetchedData.data !== "undefined" ? (
+        <div className="itemCardContainer">
+          {fetchedData.data.docs.map((item, i) => {
+            console.log(item)
+            return <><Card item={item}/></>;
+          })}
+        </div>
+      ) : (
+        <>Loading</>
+      )}
     </>
-)
-}
+  );
+};
 
 export default Home;
