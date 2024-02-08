1792c50679c4d1e8107fe3eb419ec68d26b1fe7f
totomi2323 <tothtamas2323@gmail.com>
2024-02-08 16:53:00 +0000

    updated nextprevbutton style and component

--- client/src/components/subComponents/NextPrevButtons.js
+++ client/src/components/subComponents/NextPrevButtons.js
@@ -1,33 +1,66 @@
 import { useSearchParams, Link } from "react-router-dom";
+import "../../styles/prevNextButtons.css";
 
 const NextPrevButtons = (props) => {
   const { data } = props;
 
   const [searchParams, setSearchParams] = useSearchParams();
   let searchValue = searchParams.get("searchItem");
-  let pageValue = searchParams.get("page");
 
   return (
-    <div>
-      {data.hasPrevPage ? (
-        <Link
-          className="nextPrevButton"
-          to={"/?searchItem=" + searchValue + "&page=" + (data.prevPage - 1)}
-        >
-          Previous page
-        </Link>
+    <div className="nextPrevButtonContainer">
+      {searchValue ? (
+        <>
+          {data.hasPrevPage ? (
+            <Link
+              className="nextPrevButton"
+              to={
+                "/?searchItem=" + searchValue + "&page=" + (data.prevPage - 1)
+              }
+            >
+              <span>Previous page</span>
+            </Link>
+          ) : (
+            <></>
+          )}
+          {data.hasNextPage ? (
+            <Link
+              className="nextPrevButton"
+              to={
+                "/?searchItem=" + searchValue + "&page=" + (data.nextPage - 1)
+              }
+            >
+              <span>Next page</span>
+            </Link>
+          ) : (
+            <></>
+          )}
+        </>
       ) : (
-        <></>
-      )}
-      {data.hasNextPage ? (
-        <Link
-          className="nextPrevButton"
-          to={"/?searchItem=" + searchValue + "&page=" + (data.nextPage - 1)}
-        >
-          Next page
-        </Link>
-      ) : (
-        <></>
+        <>{data.hasPrevPage ? (
+            <Link
+              className="nextPrevButton"
+              to={
+                "/?page=" + (data.prevPage - 1)
+              }
+            >
+              <span>Previous page</span>
+            </Link>
+          ) : (
+            <></>
+          )}
+          {data.hasNextPage ? (
+            <Link
+              className="nextPrevButton"
+              to={
+                "/?page=" + (data.nextPage - 1)
+              }
+            >
+              <span>Next page</span>
+            </Link>
+          ) : (
+            <></>
+          )}</>
       )}
     </div>
   );
