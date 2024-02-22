f2ab85d2c16180f96aea015c6c319ff8ced9f3b7
totomi2323 <tothtamas2323@gmail.com>
2024-02-22 17:22:33 +0000

    added style for login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,20 +1,33 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import GoogleAuthButton from "./GoogleAuthButton";
+import "../styles/login.css";
 
 const Login = () => {
   return (
-    <div>
+    <div className="login-container">
       <h1>Login/Register</h1>
-      <form action="#">
-        <label htmlFor="email">Email: </label>
-        <input type={"text"} name="email" required></input>
-        <label htmlFor="password">Password:</label>
-        <input type={"password"} name="password" required></input>
+      <form className="form-container" action="#">
+        <div className="form-group">
+          <label htmlFor="email">Email: </label>
+          <input type={"text"} name="email" required></input>
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input type={"password"} name="password" required></input>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Login
+        </button>
       </form>
-      <Link to="/register">Create account</Link>
+      <div className="register-link">
+        <Link to="/register">Create account</Link>
+      </div>
 
       <GoogleAuthButton />
+      <div>
+        <button className="btn btn-facebook">Facebook login</button>
+      </div>
     </div>
   );
 };
