6bddb099de525b5b330ab6c58799e31b7622b6f4
totomi2323 <tothtamas2323@gmail.com>
2024-07-22 18:57:36 +0100

    checking new route

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -10,7 +10,7 @@ const NavBar = () => {
   return (
     <div className="navbar">
       
-      <Link className="homeLink" to={"/"}>
+      <Link className="homeLink" to={"/index"}>
         Home
       </Link>
       {!loggedIn ? (
--- client/src/components/Router.js
+++ client/src/components/Router.js
@@ -18,7 +18,7 @@ const Router = () => {
       element: <NavBarWrapper />,
       children: [
         {
-          path: "/",
+          path: "/index",
           element: <Home />,
         },
         {
