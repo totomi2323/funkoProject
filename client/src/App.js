75da9a67640747653cba32be776d8a5cdd0ecd86
totomi2323 <tothtamas2323@gmail.com>
2024-07-29 14:21:10 +0100

    Test

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Router from "./components/Router";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./App.css"
+
 function App() {
   return (
       <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
